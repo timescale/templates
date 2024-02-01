114492faed23f5e54331e612d7d78cd8b41d62ae
Jônatas Davi Paganini <jonatasdp@gmail.com>
2024-02-01 10:11:05 -0300

    Chain refreshes

--- ticks.sql
+++ ticks.sql
@@ -5,14 +5,19 @@ CREATE TABLE symbols (
     last_price_at TIMESTAMPTZ
 );
 
-CREATE TABLE ticks (
+DROP MATERIALIZED VIEW IF EXISTS ohlcv_1d cascade;
+DROP MATERIALIZED VIEW IF EXISTS ohlcv_1h cascade;
+DROP MATERIALIZED VIEW IF EXISTS ohlcv_1m cascade;
+DROP TABLE IF EXISTS ticks CASCADE;
+
+CREATE TABLE IF NOT EXISTS ticks (
     time TIMESTAMPTZ NOT NULL,
     symbol TEXT NOT NULL REFERENCES symbols(symbol),
     price NUMERIC,
     volume NUMERIC
 );
 
-SELECT create_hypertable('ticks', by_range('time', INTERVAL '1 day'));
+SELECT create_hypertable('ticks', by_range('time', INTERVAL '1 day'), if_not_exists => true);
 
 CREATE OR REPLACE FUNCTION update_last_price()
 RETURNS TRIGGER AS $$
@@ -47,7 +52,7 @@ SELECT time_bucket('1m', time) AS bucket,
        symbol,
        candlestick_agg(time, price, volume) AS candlestick
 FROM ticks
-GROUP BY 1, 2;
+GROUP BY 1, 2 WITH NO DATA;
 
 CREATE MATERIALIZED VIEW ohlcv_1h
 WITH (timescaledb.continuous) AS
@@ -55,4 +60,27 @@ SELECT time_bucket('1h', bucket) AS bucket,
        symbol,
        rollup(candlestick) as candlestick
 FROM ohlcv_1m
-GROUP BY 1, 2;
+GROUP BY 1, 2 WITH NO DATA;
+
+CREATE MATERIALIZED VIEW ohlcv_1d
+WITH (timescaledb.continuous) AS
+SELECT time_bucket('1d', bucket) AS bucket,
+       symbol,
+       rollup(candlestick) as candlestick
+FROM ohlcv_1h
+GROUP BY 1, 2 WITH NO DATA;
+
+CREATE OR REPLACE PROCEDURE refresh_all_caggs(job_id int, config jsonb)
+LANGUAGE PLPGSQL AS $$
+BEGIN
+  CALL refresh_continuous_aggregate('ohlcv_1m', NULL, NULL);
+  COMMIT;
+  CALL refresh_continuous_aggregate('ohlcv_1h', NULL, NULL);
+  COMMIT;
+  CALL refresh_continuous_aggregate('ohlcv_1d', NULL, NULL);
+  COMMIT;
+END;
+$$;
+
+SELECT add_job('refresh_all_caggs', '5 sec');
+
